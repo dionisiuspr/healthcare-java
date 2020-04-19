b4e219a44815be0ba0629a5a61aac758db9a1d44
Dionisius Pratama <dionisiuspr@gmail.com>
2020-04-19 13:55:40 +0700

    Initial commit from spring initializr

---
+++ src/main/java/com/example/healthcare_java/HealthcareJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthcare_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareJavaApplication.class, args);
+	}
+
+}
