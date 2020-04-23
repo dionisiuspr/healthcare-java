4ba98b159393c974308a6ba9aa3320c7edc8245c
richardenilson <denilson.richardjc@gmail.com>
2020-04-23 22:06:00 +0700

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -41,14 +41,19 @@ CREATE TABLE medical_record(
 	patient_id int,
 	medstaff_id int,
 	disease_id int,
-	medicine_id int,
-	hospital_id int,
 	anamnesia VARCHAR(500),
 	PRIMARY KEY(record_id),
 	FOREIGN KEY(patient_id) REFERENCES patient(patient_id),
 	FOREIGN KEY(medstaff_id) REFERENCES medical_staff(medstaff_id),
-	FOREIGN KEY(disease_id) REFERENCES disease(disease_id),
-	FOREIGN KEY(medicine_id) references medicine(medicine_id),
+	FOREIGN KEY(disease_id) REFERENCES disease(disease_id)
+);
+
+CREATE TABLE medicine_list(
+	medicinelist_id int,
+	-- fk
+	record_id int,
+	medicine_id int,
+	PRIMARY KEY(medicinelist_id),
 	FOREIGN KEY(record_id) REFERENCES medical_record(record_id),
 	FOREIGN KEY(medicine_id) REFERENCES medicine(medicine_id)
 );
