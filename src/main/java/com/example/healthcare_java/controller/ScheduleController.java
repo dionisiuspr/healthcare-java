6b69fe4cd7da1ce1f5f12a3904fa71e2015f6e5f
irvandigian <44873309+irvandigian@users.noreply.github.com>
2020-04-24 20:10:42 +0700

    schedule

--- src/main/java/com/example/healthcare_java/controller/ScheduleController.java
+++ src/main/java/com/example/healthcare_java/controller/ScheduleController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping(path = "/healthcare")
 public class ScheduleController {
     @Autowired
     private ScheduleRepository scheduleRepository;
